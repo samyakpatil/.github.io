5479e6d08fc15437e4867495baf041c9393fa903
samyakpatil <78313037+samyakpatil@users.noreply.github.com>
2023-02-15 01:35:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/download1.jpg");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Here we will talk about Stoic history, ${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
+  function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+  
+  
